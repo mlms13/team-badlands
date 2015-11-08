5cddc158085b4fbe333c2ec2838dc2a91563a931
jvalle <jcvalle@mail.usf.edu>
2015-11-08 13:11:24 -0800

    shake it off, shake it off

--- client/js/modules/actions.js
+++ client/js/modules/actions.js
@@ -1,23 +1,34 @@
+var gameEl = document.getElementById('game');
 
 function ui (game, action) {
-	if (action === 'blackandwhite') {
-		game.game.world.filters = [game.grayFilter];
-	} else if (action === 'color') {
-		game.game.world.filters = null;
-	}
+    if (action === 'blackandwhite') {
+        game.game.world.filters = [game.grayFilter];
+    } else if (action === 'color') {
+        game.game.world.filters = null;
+    }
+
+    if (action === 'shake') {
+        gameEl.classList.add('shake');
+
+        setTimeout(function () {
+            gameEl.classList.remove('shake');
+        }, 350);
+
+        delete game.actions.ui;
+    }
 }
 
 function weather (game, action) {
 }
 
 function character (game, action) {
-	if (action === 'trump') {
-		game.character.loadTexture('trump');
-	} else if (action === 'mario') {
-		game.character.loadTexture('mario');
-	} else if (action === 'luigi') {
-		game.character.loadTexture('luigi');
-	}
+    if (action === 'trump') {
+        game.character.loadTexture('trump');
+    } else if (action === 'mario') {
+        game.character.loadTexture('mario');
+    } else if (action === 'luigi') {
+        game.character.loadTexture('luigi');
+    }
 }
 
 function powerup (game, action) {
@@ -25,8 +36,8 @@ function powerup (game, action) {
 }
 
 module.exports = {
-	ui        : ui,
-	weather   : weather,
-	character : character,
-	powerup   : powerup
+    ui        : ui,
+    weather   : weather,
+    character : character,
+    powerup   : powerup
 };
