165c58828e0d6d4faa42d1cb13242e4b4cd9aadc
Stephen Zuniga <stephen.zuniga001@gmail.com>
2015-11-07 22:44:48 -0500

    put the game in the right place

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,7 @@ var Phaser = window.Phaser;
 
 socket.init();
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO);
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
 game.state.add('Boot', require('./states/boot'));
 game.state.add('Preloader', require('./states/preloader'));
