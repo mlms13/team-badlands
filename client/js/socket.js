1bc576fb842eadb45020a5637c319120c9953019
Stephen Zuniga <stephen.zuniga001@gmail.com>
2015-11-08 18:24:27 -0500

    tweet timeouts

--- client/js/socket.js
+++ client/js/socket.js
@@ -2,16 +2,23 @@ var tweetDOM = document.querySelector('.live-tweet'),
     socket;
 
 function init () {
+    var timeout;
 	socket = io.connect('http://localhost:' + window.port);
 
 	socket.on('tweet', function (data) {
+
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = undefined;
+        }
+
         tweetDOM.innerHTML = data.tweet;
 
         tweetDOM.firstChild.classList.add('tweet--visible');
 
-        setTimeout(function() {
+        timeout = setTimeout(function() {
             tweetDOM.firstChild.classList.remove('tweet--visible');
-        }, 2000);
+        }, 2500);
 	});
 
 	return socket;
